fb3d11dc986522fc0d169f943a66ffafe597a10c
Dominik Szmek <do@minik.pl>
2018-03-30 17:46:25 +0200

    filename change

--- db.py
+++ db.py
@@ -1,32 +1,49 @@
 import os
 import sys
 import psycopg2
+import json
 
 # import ipdb; ipdb.set_trace()
 
-
 class DB():
-    DBNAME = os.environ['DBNAME']
-    DBUSER = os.environ['DBUSER']
-    DBHOST = os.environ['DBHOST']
+
+    with open('database.json') as f:
+        config = json.loads(f.read())
 
     def __init__(self):
-        try:
-            self.conn = psycopg2.connect(
-                database=self.DBNAME,
-                user=self.DBUSER,
-                host=self.DBHOST)
-        except:
-            print('db connect error')
-            sys.exit(1)
-            
+        self.conn = self.connect(
+            self.config['adm']['NAME'],
+            self.config['adm']['USER'],
+            self.config['adm']['HOST'])
+        self.conn2 = self.connect(
+            self.config['nie']['NAME'],
+            self.config['nie']['USER'],
+            self.config['nie']['HOST'])
         self.cur = self.conn.cursor()
+        self.cur2 = self.conn2.cursor()
+
+    def connect(self, name, user, host):
+        try:
+            return psycopg2.connect(
+                database=name,
+                user=user,
+                host=host)
+        except:
+            print('db {} at {} connect error'.format(name, host))
+            sys.exit(1)
 
     def query(self, query):
         self.cur.execute(query)
         return self.cur.fetchall()
-            
+
+    def change_status(self, id, status):
+        self.cur.execute("update est set status={} where id={}".format(status, id))
+        self.cur2.execute("update oferty_est set status={} where id={}".format(status, id))
+        self.conn.commit()
+        self.conn2.commit()
+
     def close(self):
         self.cur.close()
         self.conn.close()
-
+        self.cur2.close()
+        self.conn2.close()
---
+++ exclusive_contracts_notification.py
@@ -0,0 +1,81 @@
+#!/usr/bin/env python3
+
+import sys
+import os.path
+from send_email import Send_Email
+from string import Template
+from db import DB
+
+# import ipdb; ipdb.set_trace()
+
+def main():
+
+    # initialize database
+    db = DB()
+    
+    # get contracts close to expire 
+    week_left = db.query("""select id from est where wyl > now() and 
+    wyl < now() + '1 week'::interval and status=0""")
+
+    # get expired contracts
+    expired = db.query("""select id from est where wyl < now() and status=0""")
+
+    if not week_left and not expired:
+        print("brak ofert")
+        exit(0)
+
+    week_left_ids = ''
+    expired_ids = ''
+    
+    for row in week_left:
+        week_left_ids += "- {} \n".format(row[0])
+
+    for row in expired:
+        expired_ids += "- {} \n".format(row[0])
+        # change contract status to suspended
+        db.change_status(row[0], 4) # 4 - suspended
+
+    # generate mail
+    body = generate_mail(week_left_ids, expired_ids)
+
+    subject = 'kończące się wyłączności' 
+
+    # notify users by mail
+    notify(db, subject, body)
+
+
+def generate_mail(week_left_ids, expired_ids):
+
+    # load template
+    dirname = os.path.dirname(os.path.abspath(__file__))
+    body_file = open(os.path.join(dirname, 'templates', 'notification.txt'), 'r')
+    body_content = body_file.read()
+
+    # substitute variables
+    body_template = Template(body_content)
+    body = body_template.substitute(
+        week_left_ids=week_left_ids,
+        expired_ids=expired_ids)
+
+    return body
+
+
+def notify(db, subject, body):
+
+    # initialize send mail class
+    send_email = Send_Email()
+
+    # get user email to notify
+    emails = db.query("""select nazwa_pelna, email2 from users where aktywna='t'""")
+    email_to = [email[0] + ' <' + email[1] + '>' for email in emails]
+
+    # test
+    email_to = ['domino@dominowisla.pl']
+    
+    # send mail to active users
+    send_email.send(email_to, subject, body)
+    send_email.quit()
+
+
+if __name__ == '__main__':
+    main()
